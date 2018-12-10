93eb53e6ab48c709d1f31daf6deaf47b1d679890
Vladimir Pligos <pligosv@gmail.com>
2018-12-10 20:41:37 +0300

    delete all words from db when open and add test words

--- app/src/main/java/com/example/vpligos/roomwordsample/WordRoomDatabase.java
+++ app/src/main/java/com/example/vpligos/roomwordsample/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.example.vpligos.roomwordsample;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Word.class}, version = 1)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -13,15 +16,43 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     private static volatile WordRoomDatabase INSTANCE;
 
     static WordRoomDatabase getDatabase(final Context context) {
-        if(INSTANCE == null) {
-            synchronized (WordRoomDatabase.class){
-                if(INSTANCE == null) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
+                            .addCallback(roomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback roomDatabaseCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+    private static class PopulateDbAsync extends AsyncTask<Void,Void,Void> {
+        private final WordDao dao;
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            dao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            dao.deleteAll();
+            Word word = new Word("Hello!");
+            dao.insert(word);
+            word = new Word("world!");
+            dao.insert(word);
+            return null;
+        }
     }
 }
